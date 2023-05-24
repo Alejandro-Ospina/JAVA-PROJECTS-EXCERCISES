60badaca70887821d67d26683c467e400191fb3e
Alejandro Ospina Castaño <aopsina@utp.edu.co>
2023-05-23 19:08:35 -0500

    Sexto commit

---
+++ Prueba Java - Encuentro 30 (equipo 40)/HolaMundo/src/holamundo/Ejercicio4.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+//  @author new53
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        String nombre = "Alejandro Ospina";
+        int edad = 27;
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("La edad es: " + edad);
+    }
+}
---
+++ Prueba Java - Encuentro 30 (equipo 40)/HolaMundo/src/holamundo/EjercicioDeteccionErroes.java
@@ -0,0 +1,12 @@
+
+package holamundo;
+
+//  @author new53
+public class EjercicioDeteccionErroes {
+
+    public static void main(String[] args) {
+        String nombre;
+        boolean bandera;
+        char Char;
+    }
+}
